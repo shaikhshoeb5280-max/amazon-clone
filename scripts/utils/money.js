888a1d2143b33fd3496230ee9968220386606b67
Shoieb shaikh <shaikhshoeb5280@gmail.com>
2026-02-24 15:28:43 +0530

    testing2

--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 export function formatCurrency (priceCents){
  return (Math.round(priceCents)/100).toFixed(2)
 }
 
-=======
-export function formatCurrency (priceCents){
- return (Math.round(priceCents)/100).toFixed(2)
-}
-
->>>>>>> 2b8896f7e26f7b2ebe62065981dfe16282a1cfa2
 //export default formatCurrency can us this if only one thing is expoting from a file 
